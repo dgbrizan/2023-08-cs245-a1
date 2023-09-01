9284a79684c31e57da8c754915eb9dff75459d0a
dgbrizan <32884156+dgbrizan@users.noreply.github.com>
2023-09-01 15:45:59 -0700

    Adding starter code for two sections of the assignment

---
+++ QuickerThanQuicksort.java
@@ -0,0 +1,22 @@
+/**
+ * Your documentation goes here
+ */
+public class QuickerThanQuicksort {
+
+	public int quicksort(double [] arr, int left, int right) {
+		// TODO: Add your code here...
+
+		return 0; // TODO: Change this return value.
+	}
+
+	public void quadraticsort(double [] arr, int left, int right) {
+	}
+
+
+	// TODO: Add additional functions?
+
+
+	public static void main (String [] args) {
+		// TODO: Add your "main" code here to generate a random array and sort it
+	}
+}
---
+++ SortingAlgorithms.java
@@ -0,0 +1,19 @@
+/**
+ * Your documentation goes here
+ */
+public class SortingAlgorithms {
+
+	public long sort(String algorithm, float [] arr) {
+		// TODO: Add your code here...
+
+		return 0; // TODO: Change this return value.
+	}
+
+
+	// TODO: Add additional functions!
+
+
+	public static void main (String [] args) {
+		// TODO: Add your "main" code here
+	}
+}
